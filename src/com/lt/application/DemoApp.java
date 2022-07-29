89868ce1ea38f8b11822e62c56f113e4af303a1b
Narendra <narendra017>
2022-07-29 12:10:56 +0530

    first commit is done

---
+++ src/com/lt/application/DemoApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.lt.application;
+
+/**
+ * @author DELL
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("This is my first program");
+	}
+
+}
